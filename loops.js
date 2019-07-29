da031904c9352583a20ae52586398dc9ffdca618
anya litvinova <anya.s.litvinova@gmail.com>
2019-07-29 19:24:44 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,32 @@
+function forLoop(array){
+  let newArray = [];
+  for(let i = 0; i <= 25; i++){
+    if(newArray[i] === 1){
+      newArray += "I am 1 strange loop.";
+    } else {
+      newArray += "I am ${i} strange loop.";
+  }
+} return newArray;
+}
+
+function whileLoop(n){
+  while(n > 0){
+    console.log(--n)
+  }
+  return 'done';
+}
+
+function doWhileLoop(num){
+  let i = 0;
+ function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+
+do {
+  console.log("I run once regardless.");
+} while (incrementVariable() < num);
+}
+  
+  
+  

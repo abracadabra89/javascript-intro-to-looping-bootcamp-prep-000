9783b64e278f0edba5470e00a004efddbc486e63
anya litvinova <anya.s.litvinova@gmail.com>
2019-07-29 19:35:12 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,12 +1,13 @@
 function forLoop(array){
-  for(let i = 0; i < 25; i++){
-  if (i === 1) {
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
       array.push("I am 1 strange loop.");
-    } else {
+    }
+    else {
       array.push("I am ${i} strange loops.");
     }
+  }
   return array;
-}
 }
 
 function whileLoop(n){
